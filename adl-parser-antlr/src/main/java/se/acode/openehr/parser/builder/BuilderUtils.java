577baafef66d2f72c7d4b6676a4954a6922500f0
BertVerhees <bert.verhees@rosa.nl>
2017-06-29 12:22:13 +0200

    refactoring grammar

--- adl-parser-antlr/src/main/java/se/acode/openehr/parser/builder/BuilderUtils.java
+++ adl-parser-antlr/src/main/java/se/acode/openehr/parser/builder/BuilderUtils.java
@@ -1,9 +1,13 @@
 package se.acode.openehr.parser.builder;
 
 import org.openehr.rm.datatypes.text.CodePhrase;
+import se.acode.openehr.parser.errors.ArchetypeBuilderError;
 import se.acode.openehr.parser.v1_4.ArchetypeParser;
 import se.acode.openehr.parser.errors.ArchetypeADLErrorListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -33,5 +37,64 @@ public class BuilderUtils {
         value = value.substring(1, value.length() - 1);
         map.put(keyString, value);
     }
+
+    static String handleSingleStringItem(String string, String item, ArchetypeParser.Attr_valContext attrValContext, String itemDesciption, ArchetypeADLErrorListener errorListener ){
+        if (string == null) {
+            string = item;
+        } else {
+            errorListener.getParserErrors().addError(ArchetypeBuilderError.buildMessage(attrValContext, "There can be only one "+itemDesciption+"-section per description."));
+        }
+        return string;
+    }
+
+    static List<String> handleSingleStringItemList(List<String> strings, String item, ArchetypeParser.Attr_valContext attrValContext, String itemDesciption, ArchetypeADLErrorListener errorListener ){
+        if (strings == null) {
+            strings = new ArrayList<>();
+            String keyword = item;
+            strings.add(keyword);
+        } else {
+            errorListener.getParserErrors().addError(ArchetypeBuilderError.buildMessage(attrValContext, "There can be only one "+itemDesciption+"-section per resource-description-item."));
+        }
+        return strings;
+    }
+
+    static Map<String, String> handleStringStringMap(Map<String, String> map, ArchetypeParser.Attr_valContext attrValContext, String itemDesciption, ArchetypeADLErrorListener errorListener ){
+        if (map == null) {
+            map = new HashMap<>();
+        } else {
+            errorListener.getParserErrors().addError(ArchetypeBuilderError.buildMessage(attrValContext, "There can be only one "+itemDesciption+"-section per resource-description-item."));
+        }
+        for (ArchetypeParser.Keyed_objectContext keyedObjectContext : attrValContext.object_block().object_value_block().keyed_object()) {
+            ArchetypeParser.Object_value_blockContext o = keyedObjectContext.object_block().object_value_block();
+            ArchetypeParser.String_valueContext sa = keyedObjectContext.primitive_value().string_value();
+            addEntry(sa, o, map, errorListener);
+        }
+        return map;
+    }
+
+    static List<String> handleStringItemList(List<String> strings, ArchetypeParser.Attr_valContext attrValContext, String itemDesciption, ArchetypeADLErrorListener errorListener ){
+        if (strings == null) {
+            strings = new ArrayList<>();
+        } else {
+            errorListener.getParserErrors().addError(ArchetypeBuilderError.buildMessage(attrValContext, "There can be only one "+itemDesciption+"-section per resource-description-item."));
+        }
+
+        for (ArchetypeParser.Primitive_valueContext primitiveValueContext : attrValContext.object_block().object_value_block().primitive_object().primitive_list_value().primitive_value()) {
+            String string = primitiveValueContext.string_value().STRING().getText();
+            string = string.substring(1, string.length() - 1);
+            strings.add(string);
+        }
+        return strings;
+    }
+
+    static CodePhrase handleSingleCodePhraseItem(CodePhrase codePhrase, String item, ArchetypeParser.Attr_valContext attrValContext, String itemDesciption, ArchetypeADLErrorListener errorListener ){
+        if (codePhrase == null) {
+            codePhrase = BuilderUtils.returnCodePhraseFromTermCodeRefString(attrValContext.object_block().object_value_block().primitive_object().primitive_value().term_code_value().TERM_CODE_REF().getText(), errorListener);;
+        } else {
+            errorListener.getParserErrors().addError(ArchetypeBuilderError.buildMessage(attrValContext, "There can be only one "+itemDesciption+"-section per description."));
+        }
+        return codePhrase;
+    }
+
 
 }
